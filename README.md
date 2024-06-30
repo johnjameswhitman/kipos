d90792b965a359e83f421b211001b3da817ce75d
John Whitman <john@s2sq.com>
2024-06-30 16:54:30 -0400

    Adds real basic config.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Kipos
+
+Collection of system configs for my machines.
+
+## TODO
+
+- [x] Configure a basic `test` VM with flake
+- [x] Build `test` VM locally (ref: [_Setting up qemu VM using nix flakes_][nix_vm_gist])
+    ```shell
+    nixos-rebuild build-vm --flake .#test
+    QEMU_NET_OPTS="hostfwd=tcp::2221-:22" result/bin/run-nixos-vm
+    # VM will run in the terminal where you started it, but you can also SSH in:
+    ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no admin@localhost -p 2221
+    # Once done, hit CTRL+a,x to shut down VM
+    ```
+- [ ] Build `test` VM in GHA
+
+[nix_vm_gist]: https://gist.github.com/FlakM/0535b8aa7efec56906c5ab5e32580adf
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Kipos flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    disko,
+    nixpkgs,
+    self,
+    sops-nix,
+    ...
+  }: let
+    system = "x86_64-linux"; # Later can add support for darwin as-needed.
+    pkgs = import nixpkgs {
+      system = "${system}";
+      config.allowUnfree = true;
+    };
+  in {
+    formatter.${system} = pkgs.alejandra;
+
+    # test is a hostname for our machine
+    nixosConfigurations.test = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
