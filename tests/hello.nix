045da97b3e8f96ea9d2542d34fa39788eff568a5
John Whitman <john@s2sq.com>
2024-10-07 20:23:41 -0400

    Cleans up test.

--- tests/hello.nix
+++ tests/hello.nix
@@ -1,15 +1,15 @@
 {pkgs, ...}: {
   name = "hello";
   nodes = {
-    machine1 = {pkgs, ...}: {
+    machine = {pkgs, ...}: {
       imports = [../machines/hello.nix];
       environment.systemPackages = [pkgs.hello];
     };
   };
   testScript = ''
     start_all()
-    machine1.wait_for_unit("sshd.service")
-    machine1.succeed("hello")
-    machine1.fail("hello")
+    machine.wait_for_unit("sshd.service")
+    machine.succeed("hello")
+    machine.succeed("ssh-keygen -t rsa -N ''' -f ~/.ssh/id_rsa")
   '';
 }
