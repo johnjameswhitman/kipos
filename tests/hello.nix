89526cc696cb697aa6e0261223b93c4f0e47ea61
John Whitman <johnjameswhitman@gmail.com>
2025-01-24 18:17:04 -0500

    Gets rid of missing service in test.

--- tests/hello.nix
+++ tests/hello.nix
@@ -11,7 +11,8 @@
   };
   testScript = ''
     start_all()
-    machine1.wait_for_unit("sops-nix.service")
+    # machine1.wait_for_unit("sops-nix.service")
+    machine1.wait_for_unit("sshd.service")
     machine1.succeed("hello")
     # machine1.succeed("[[ $(cat /run/secrets/hello) == world ]]")
     machine1.succeed("[[ $(cat /etc/hello) == world ]]")
