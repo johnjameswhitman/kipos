75b6f1cadb3ae56de8b64dfe8f54c33949c33902
John Whitman <johnjameswhitman@gmail.com>
2025-01-25 01:35:17 -0500

    Tries next next next last thing.

--- tests/hello.nix
+++ tests/hello.nix
@@ -35,7 +35,7 @@
   };
   testScript = ''
     start_all()
-    machine1.wait_for_unit("sops-nix.service")
+    # machine1.wait_for_unit("sops-nix.service")
     machine1.wait_for_unit("sshd.service")
     machine1.succeed("hello")
     machine1.succeed("[[ $(cat /etc/hello) == world ]]")
